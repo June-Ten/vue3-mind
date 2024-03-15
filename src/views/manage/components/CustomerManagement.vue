9db3b21a1c672839c85e87487c2362661518da84
wangqiling <891696668@qq.com>
2024-03-15 15:05:12 +0800

    管理页布局完成

--- src/views/manage/components/CustomerManagement.vue
+++ src/views/manage/components/CustomerManagement.vue
@@ -3,3 +3,10 @@
     客户管理
   </div>
 </template>
+
+<style lang="less" scoped>
+.customer-management-box {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/manage/components/SalesManagement.vue
+++ src/views/manage/components/SalesManagement.vue
@@ -3,3 +3,10 @@
     销售管理
   </div>
 </template>
+
+<style lang="less" scoped>
+.sales-management-box {
+  width: 100%;
+  height: 100%;
+}
+</style>
