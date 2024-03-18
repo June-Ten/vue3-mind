124bae24380a5c59b981695d6105b2fa61ff4cce
wangqiling <891696668@qq.com>
2024-03-18 14:17:32 +0800

    company info

--- src/views/manage/components/CardDetail.vue
+++ src/views/manage/components/CardDetail.vue
@@ -76,6 +76,67 @@
         </div>
       </a-col>
     </a-row>
+    <div class="company-info-box mgn-top">
+      <a-row>
+        <a-col :span="8" style="display: flex;">
+          <div class="company-info__label">信用代码: </div>
+          <div class="company-info__value line-1">914403006189074649</div>
+        </a-col>
+        <a-col :span="8" style="display: flex;">
+          <div class="company-info__label">电话：</div>
+          <div class="company-info__value line-1">18218148072</div>
+        </a-col>
+        <a-col :span="8" style="display: flex;">
+          <div class="company-info__label">国标行业：</div>
+          <div class="company-info__value line-1">综合零售</div>
+        </a-col>
+      </a-row>
+      <a-row class="mgn-top">
+        <a-col :span="8" style="display: flex;">
+          <div class="company-info__label">法定代表人：</div>
+          <div class="company-info__value line-1">方晓峰</div>
+        </a-col>
+        <a-col :span="8" style="display: flex;">
+          <div class="company-info__label">邮箱：</div>
+          <div class="company-info__value line-1">18218148072@163.com</div>
+        </a-col>
+        <a-col :span="8" style="display: flex;">
+          <div class="company-info__label">规模：</div>
+          <div class="company-info__value line-1">超大型</div>
+        </a-col>
+      </a-row>
+      <a-row class="mgn-top">
+        <a-col :span="8" style="display: flex;">
+          <div class="company-info__label">注册资本：</div>
+          <div class="company-info__value line-1">16598万美元</div>
+        </a-col>
+        <a-col :span="8" style="display: flex;">
+          <div class="company-info__label">网址：</div>
+          <div class="company-info__value line-1 company-info__value-link">xxxxxxxxxxxxxxxxxx.com</div>
+        </a-col>
+      </a-row>
+      <a-row class="mgn-top">
+        <a-col :span="8" style="display: flex;">
+          <div class="company-info__label">成立日期：</div>
+          <div class="company-info__value line-1">1995.12.08</div>
+        </a-col>
+        <a-col :span="8" style="display: flex;">
+          <div class="company-info__label">地址：</div>
+          <div
+            class="company-info__value company-info__value-link"
+          >
+            北京市罗湖区黄贝街道凤凰社区凤凰路126号中山花园二层1号
+          </div>
+        </a-col>
+      </a-row>
+      <a-divider style="margin: 10px 0;" />
+      <a-row>
+        <a-col :span="24" style="font-size: 12px;color: #1d2129;">
+          <span>简介：</span>
+          <span>航天信息股份有限公司，成立于1995年，位于北京市，是一家工业软件开发、大型系统集成服务商。企业注册资本16598万美元，超过了100%的广东省同行，实缴资本16598万美元。</span>
+        </a-col>
+      </a-row>
+    </div>
   </div>
 </template>
 
@@ -196,6 +257,22 @@
         font-size: 10px;
         padding: 2px 5px;
       }
+    }
+  }
+  .company-info-box {
+    width: 100%;
+    background-color: rgba(248, 251, 255, 1);
+    padding: 10px;
+    font-size: 12px;
+    .company-info__label {
+      flex-shrink: 0;
+      color: #86909c;
+    }
+    .company-info__value {
+      color: #1d2129;
+    }
+    .company-info__value-link {
+      color: #3964f5;
     }
   }
 }
